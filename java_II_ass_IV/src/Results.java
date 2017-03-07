bcfd18a873704b2219b204157cede55de2e190ec
powmonk <dan@aldron.co.uk>
2017-03-07 20:45:01 +0000

    Bubble sort functional

--- java_II_ass_IV/src/Results.java
+++ java_II_ass_IV/src/Results.java
@@ -1,21 +1,58 @@
 
 public class Results {
+	private static void alphabetiseArray() {
+		boolean done = false;
+		String[] workAry = result;
+		int count = workAry.length-1;
+		int floor = 0;
+		int word = 1;
+		int letter = 0;
+
+		while(!done){
+			if(count > floor){
+				int a = (int)Character.toUpperCase(workAry[count].split(" |	")[word].charAt(letter));
+				int b = (int)Character.toUpperCase(workAry[count-1].split(" |	")[word].charAt(letter));
+				
+				if(a<b){
+//					System.out.println("comparison hit");
+					String cache = workAry[count-1];
+					workAry[count-1] = workAry[count];
+					workAry[count] = cache;
+				}
+				
+				count--;	
+			}else if(floor == workAry.length){
+				if(word == 0){
+					word = 1;
+					letter = 1;
+					floor = 0;
+					count = workAry.length-1;
+				
+				}else{
+					done = true;
+				}
+//				done = true;
+			}else{
+				floor++;
+				count = workAry.length-1;
+			}
+		}
+		result = workAry;
+	}
+	
+
 	public static void main(String[] args){
-		result = ReadFileAsArray.readFile(args[0]);
+		result = ReadFileAsArray.readFile("marks.txt");
 		
-		result = alphabetiseArray(result);
+		alphabetiseArray();
+		
+		for(int i=0;i<result.length;i++){
+			//System.out.println((int)Character.toUpperCase(result[i].split(" |	")[1].charAt(0)));
+			System.out.println(result[i]);
+		}
 		
 	}
 	
-	private static String[] alphabetiseArray(String[] result) {
-		for(int i=0;i<result.length;i++){
-			String[] temp = result[i].split(" |	");
-			bullshit[i] = temp;
-		}
-		
-		return null;
-	}
-	private static String[][] bullshit;
 	private static String[] result;
 	
 }
